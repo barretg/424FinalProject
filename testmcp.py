2eb25dae2d128dcc75a0b0f8c73c99636d95d2df
Barret Glass <bmg7@rice.edu>
2023-11-15 16:29:40 +0000

    Testing code... not working

--- testmcp.py
+++ testmcp.py
@@ -2,6 +2,7 @@
 # SPDX-License-Identifier: MIT
 
 import board
+import time
 import adafruit_mcp4728
 
 MCP4728_DEFAULT_ADDRESS = 0x60
@@ -10,11 +11,29 @@ MCP4728A4_DEFAULT_ADDRESS = 0x64
 i2c = board.I2C()  # uses board.SCL and board.SDA
 # i2c = board.STEMMA_I2C()  # For using the built-in STEMMA QT connector on a microcontroller
 #  use for MCP4728 variant
-mcp4728 = adafruit_mcp4728.MCP4728(i2c, adafruit_mcp4728.MCP4728_DEFAULT_ADDRESS)
+#mcp4728 = adafruit_mcp4728.MCP4728(i2c, adafruit_mcp4728.MCP4728_DEFAULT_ADDRESS)
 #  use for MCP4728A4 variant
-#  mcp4728 = adafruit_mcp4728.MCP4728(i2c, adafruit_mcp4728.MCP4728A4_DEFAULT_ADDRESS)
+mcp4728 = adafruit_mcp4728.MCP4728(i2c, adafruit_mcp4728.MCP4728A4_DEFAULT_ADDRESS)
 
-mcp4728.channel_a.value = 65535  # Voltage = VDD
-mcp4728.channel_b.value = int(65535 / 2)  # VDD/2
-mcp4728.channel_c.value = int(65535 / 4)  # VDD/4
-mcp4728.channel_d.value = 0  # 0V
+mcp4728.channel_a.value = 65535
+time.sleep(2);
+mcp4728.channel_a.value = 0
+
+mcp4728.channel_b.value = 65535
+time.sleep(2);
+mcp4728.channel_b.value = 0
+
+mcp4728.channel_c.value = 65535
+time.sleep(2);
+mcp4728.channel_c.value = 0
+
+mcp4728.channel_d.value = 65535
+time.sleep(2);
+mcp4728.channel_d.value = 0
+
+
+
+#mcp4728.channel_a.value = 65535  # Voltage = VDD
+#mcp4728.channel_b.value = int(65535 / 2)  # VDD/2
+#mcp4728.channel_c.value = int(65535 / 4)  # VDD/4
+#mcp4728.channel_d.value = 0  # 0V
