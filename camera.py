b9502a909194ee89d2d449cf2611f6958c0cf031
Barret Glass <bmg7@rice.edu>
2023-12-07 23:44:19 +0000

    i like pie

--- camera.py
+++ camera.py
@@ -9,7 +9,7 @@ https://github.com/nicorossirice/comp424-FinalProject/tree/main
 Hackster Webpage:
 https://www.hackster.io/team-youngerwoods/the-nicomobile-83caa9
 
-Used the following reference code: 
+Used the following reference code:
 https://www.instructables.com/Autonomous-Lane-Keeping-Car-Using-Raspberry-Pi-and/
 https://www.hackster.io/really-bad-idea/autonomous-path-following-car-6c4992
 
@@ -38,11 +38,11 @@ def detect_edges(frame):
     upper_blue = np.array([150, 255, 255], dtype="uint8")
     mask = cv2.inRange(hsv,lower_blue,upper_blue)
     #cv2.imshow("mask",mask)
-    
+
     # detect edges
     edges = cv2.Canny(mask, 50, 100)
     cv2.imshow("edges",edges)
-    
+
     return edges
 
 def region_of_interest(edges):
@@ -56,27 +56,27 @@ def region_of_interest(edges):
         (width , height/2),
         (width , height),
     ]], np.int32)
-    
+
     cv2.fillPoly(mask, polygon, 255)
-    
+
     cropped_edges = cv2.bitwise_and(edges, mask)
 #    cv2.imshow("roi",cropped_edges)
-    
+
     return cropped_edges
 
 def detect_line_segments(cropped_edges):
-    rho = 1  
-    theta = np.pi / 180  
-    min_threshold = 10  
-    
-    line_segments = cv2.HoughLinesP(cropped_edges, rho, theta, min_threshold, 
+    rho = 1
+    theta = np.pi / 180
+    min_threshold = 10
+
+    line_segments = cv2.HoughLinesP(cropped_edges, rho, theta, min_threshold,
                                     np.array([]), minLineLength=5, maxLineGap=150)
 
     return line_segments
 
 def average_slope_intercept(frame, line_segments):
     lane_lines = []
-    
+
     if line_segments is None:
         print("no line segments detected")
         return lane_lines
@@ -88,17 +88,17 @@ def average_slope_intercept(frame, line_segments):
     boundary = 1/3
     left_region_boundary = width * (1 - boundary)
     right_region_boundary = width * boundary
-    
+
     for line_segment in line_segments:
         for x1, y1, x2, y2 in line_segment:
             if x1 == x2:
                 print("skipping vertical lines (slope = infinity")
                 continue
-            
+
             fit = np.polyfit((x1, x2), (y1, y2), 1)
             slope = (y2 - y1) / (x2 - x1)
             intercept = y1 - (slope * x1)
-            
+
             if slope < 0:
                 if x1 < left_region_boundary and x2 < left_region_boundary:
                     left_fit.append((slope, intercept))
@@ -118,72 +118,72 @@ def average_slope_intercept(frame, line_segments):
 
 def make_points(frame, line):
     height, width, _ = frame.shape
-    
+
     slope, intercept = line
-    
+
     y1 = height  # bottom of the frame
     y2 = int(y1 / 2)  # make points from middle of the frame down
-    
+
     if slope == 0:
         slope = 0.1
-        
+
     x1 = int((y1 - intercept) / slope)
     x2 = int((y2 - intercept) / slope)
-    
+
     return [[x1, y1, x2, y2]]
 
 def display_lines(frame, lines, line_color=(0, 255, 0), line_width=6):
     line_image = np.zeros_like(frame)
-    
+
     if lines is not None:
         for line in lines:
             for x1, y1, x2, y2 in line:
                 cv2.line(line_image, (x1, y1), (x2, y2), line_color, line_width)
-                
+
     line_image = cv2.addWeighted(frame, 0.8, line_image, 1, 1)
-    
+
     return line_image
 
 def display_heading_line(frame, steering_angle, line_color=(0, 0, 255), line_width=5 ):
     heading_image = np.zeros_like(frame)
     height, width, _ = frame.shape
-    
+
     steering_angle_radian = steering_angle / 180.0 * math.pi
-    
+
     x1 = int(width / 2)
     y1 = height
     x2 = int(x1 - height / 2 / math.tan(steering_angle_radian))
     y2 = int(height / 2)
-    
+
     cv2.line(heading_image, (x1, y1), (x2, y2), line_color, line_width)
     heading_image = cv2.addWeighted(frame, 0.8, heading_image, 1, 1)
-    
+
     return heading_image
 
 def get_steering_angle(frame, lane_lines):
-    
+
     height,width,_ = frame.shape
-    
+
     if len(lane_lines) == 2:
         _, _, left_x2, _ = lane_lines[0][0]
         _, _, right_x2, _ = lane_lines[1][0]
         mid = int(width / 2)
         x_offset = (left_x2 + right_x2) / 2 - mid
         y_offset = int(height / 2)
-        
+
     elif len(lane_lines) == 1:
         x1, _, x2, _ = lane_lines[0][0]
         x_offset = x2 - x1
         y_offset = int(height / 2)
-        
+
     elif len(lane_lines) == 0:
         x_offset = 0
         y_offset = int(height / 2)
-        
+
     angle_to_mid_radian = math.atan(x_offset / y_offset)
-    angle_to_mid_deg = int(angle_to_mid_radian * 180.0 / math.pi)  
+    angle_to_mid_deg = int(angle_to_mid_radian * 180.0 / math.pi)
     steering_angle = angle_to_mid_deg + 90
-    
+
     return steering_angle
 
 
@@ -193,7 +193,7 @@ def get_steering_angle(frame, lane_lines):
 # background redpx values could be <1000
 # detection could be between 1000-25000
 def check_for_stop_sign(frame):
-    
+
     # Range of red in HSV
     left_lower_red = np.array([145, 135, 140], dtype="uint8")
     left_upper_red = np.array([175, 150, 200], dtype="uint8")
@@ -213,15 +213,15 @@ def check_for_stop_sign(frame):
     right_mask = cv2.inRange(frame, right_lower_red, right_upper_red)
     mask = cv2.bitwise_or(left_mask, right_mask)
     num_red_px = cv2.countNonZero(mask)
-    
+
     '''
-    Average HSV value for red construction paper: 
+    Average HSV value for red construction paper:
     125.98791666666666
     107.065
     163.78041666666667
     '''
 
-    # TESTING: 
+    # TESTING:
     center = frame[:, :, :]
     # center[:110, :, :] = 0
     # center[130:, :, :] = 0
@@ -259,12 +259,11 @@ time.sleep(2)
 
 ''' CONSTANTS (for PD algorithm) '''
 
-speed = 8 
+speed = 8
 lastTime = 0
 lastError = 0
 
 
-pwm = PWMControl()
 
 
 ''' BEGIN MAIN CODE '''
@@ -273,9 +272,6 @@ pwm = PWMControl()
 # This means the shutdown code will actually run
 done = False
 
-# TODO: set initial speed
-pwm.set_throttle(int(65535 / 1.9))
-
 
 def stop(signum, stackframe):
     global done
@@ -283,7 +279,7 @@ def stop(signum, stackframe):
 
 signal.signal(signal.SIGINT, stop)
 
-show_camera = True 
+show_camera = True
 
 
 
@@ -311,8 +307,17 @@ derivative_resp_data = []
 ''' MAIN FUNCTION '''
 num_stopped = 0
 
+pwm = PWMControl()
+pwm.set_throttle(int(65535 / 2))
+flag = True
+
 while not done:
     ret,frame = video.read()
+
+    if flag:
+        pwm.set_throttle(int(65535 / 1.8))
+        flag = False
+
 
     # Detect lanes
     edges = detect_edges(frame)
@@ -350,14 +355,15 @@ while not done:
         pwm.set_throttle(int(65535 / 2))
 
     # TODO: tweak this to desired speed:
-    speed_threshold = 7000  
+    speed_threshold = 250000
 
     if speed > speed_threshold:
-        pwm.set_throttle(int(65535 / 2))
-    elif speed < speed_threshold - 2000:
         pwm.set_throttle(int(65535 / 1.9))
+#    elif speed < speed_threshold - 60000:
+#        pwm.set_throttle(int(65535 / 1.8))
+    else:
+        pwm.set_throttle(int(65535 / 1.8))
 
-    
     ''' CALCULATE DERIVATIVE FROM PD ALGORITHM '''
     now = time.time()
     dt = now - lastTime
@@ -378,19 +384,29 @@ while not done:
     print(f"Steering angle: {steering_angle}")
     print(f"Turn amt: {turn_amt}")
 
-#max((7-turn_amt)/7.0, 1) * 
+
+#max((7-turn_amt)/7.0, 1) *
     # lower voltage is more left
     # higher voltage is more right
-    if turn_amt < 7:
+    """
+    if turn_amt < 6:
+        turn_amt = int(65535 / 1)
+    elif turn_amt < 7:
         # right
         turn_amt = int(65535 / 1.5)
     elif turn_amt < 8:
         # neutral
         turn_amt = int(65535 / 2)
+    elif turn_amt < 9:
+        turn_amt = int(65525 / 2.5)
     else:
         # left
-        turn_amt = int(65535 / 2.5)
+        turn_amt = int(65535 / 3)
     turn_amt = int(turn_amt)
+    """
+    turn_amt = turn_amt *-1 +9
+    turn_amt = turn_amt * (2/3) + 1 #value between 1 and 3
+    turn_amt = 65525 / turn_amt #actual output value
 
     print(f"Adjusted turn: {turn_amt}")
     steering_pwm_data.append(pwm.set_steering(turn_amt))
